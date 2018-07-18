acc02a0722aaaeec2e1669d0198dc4257cbced37
sobadprogrammer <lovsss1@gmail.com>
2018-07-18 20:21:11 +0200

    some styling fixes

--- app/src/App.js
+++ app/src/App.js
@@ -10,7 +10,23 @@ class App extends Component {
           <span>LOGO</span>
           <a>LOGIN/REGISTER</a>
         </nav>
+        <div className="add-note">
+          CREATE A NOTE:
+          <input id="add-note" type="text" placeholder="New note..." />
+          <button type="button">Add note</button>
+        </div>
+        <div className="featured-notes">
+          FEATURED NOTES:
+          <ul>
+            <li>Example note 1</li>
+            <li>Example note 2</li>
+            <li>Example note 3</li>
+          </ul>
+        </div>
+        <div className="my-notes">
+          My notes:
 
+        </div>
       </div>
     );
   }
